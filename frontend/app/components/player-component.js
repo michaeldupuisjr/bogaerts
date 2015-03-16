d7a4919794658551e8afb3db8a5b70c6fd672615
Michael Dupuis Jr <michael.dupuis@dockyard.com>
2015-03-16 10:07:52 -0400

    Adds `dropPlayer` action to player component.

--- frontend/app/components/player-component.js
+++ frontend/app/components/player-component.js
@@ -1,11 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  hasBeenDrafted: Ember.computed.bool('player.fantasyTeam'),
   selectedTeam: null,
   tagName: '',
 
   assignPlayerToTeam: Ember.observer('selectedTeam', function() {
     this.player.set('fantasyTeam', this.selectedTeam);
     this.player.save();
-  })
+  }),
+
+  actions: {
+    dropPlayer: function(player) {
+      player.set('fantasyTeam', null);
+      player.save();
+    }
+  }
 });
