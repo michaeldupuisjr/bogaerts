9f3d2938ba1d0ec9b462d597c21d46f8e5f0c9d5
Michael Dupuis Jr <michael.dupuis@dockyard.com>
2015-03-28 11:20:38 -0400

    Organizes players by position.

--- frontend/app/adapters/application.js
+++ frontend/app/adapters/application.js
@@ -1,10 +1,10 @@
-// import config from '../config/environment';
-// import Firebase from 'firebase';
-// import FirebaseAdapter from 'emberfire/adapters/firebase';
+import config from '../config/environment';
+import Firebase from 'firebase';
+import FirebaseAdapter from 'emberfire/adapters/firebase';
 
-// export default FirebaseAdapter.extend({
-  // firebase: new Firebase(config.firebase)
-// });
+export default FirebaseAdapter.extend({
+  firebase: new Firebase(config.firebase)
+});
 
-import DS from 'ember-data';
-export default DS.FixtureAdapter.extend({});
+// import DS from 'ember-data';
+// export default DS.FixtureAdapter.extend({});
--- frontend/app/routes/auction.js
+++ frontend/app/routes/auction.js
@@ -11,6 +11,12 @@ export default Ember.Route.extend({
     this._super(controller, model);
     this.controllerFor('auction').set('fantasyTeams', model.fantasyTeams);
     this.controllerFor('auction').set('mdFantasyTeam', model.fantasyTeams.filterBy('abbreviation', 'MD')[0]);
-    this.controllerFor('auction').set('players', model.players);
+    this.controllerFor('auction').set('catchers', model.players.filterBy('pos', 'C'));
+    this.controllerFor('auction').set('firstBasemen', model.players.filterBy('pos', '1B'));
+    this.controllerFor('auction').set('outfielders', model.players.filterBy('pos', 'OF'));
+    this.controllerFor('auction').set('secondBasemen', model.players.filterBy('pos', '2B'));
+    this.controllerFor('auction').set('shortstops', model.players.filterBy('pos', 'SS'));
+    this.controllerFor('auction').set('thirdBasemen', model.players.filterBy('pos', '3B'));
+    this.controllerFor('auction').set('designatedHitters', model.players.filterBy('pos', 'UTIL'));
   }
 });
